9fa5db9be632ec6adaa3e7400546c5471d3636c7
ibmboy19 <ibmboy19@gmail.com>
2016-05-03 14:30:06 +0800

    appMain.go refactor

--- beanfunLineageInform-go/appMain.go
+++ beanfunLineageInform-go/appMain.go
@@ -8,36 +8,18 @@ import (
 	"unsafe"
 )
 
-const PROCESS_ALL_ACCESS = 0x1f0fff
-
 func main() {
-	// Load dll
-	var userModule = syscall.NewLazyDLL("user32.dll")
-	var kernelModule = syscall.NewLazyDLL("kernel32.dll")
-
-	// Call windows api
 	// find Lineage launcher Process
-	var findWindowProcess = userModule.NewProc("FindWindowW")
-	hwnd, _, _ := findWindowProcess.Call(0, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("Lineage launcher"))))
+	isFindWindow, windowHandle := findWindow("Lineage launcher")
 
-	if hwnd <= 0 {
-		fmt.Println("Window not found!")
-	} else {
-		// get Process id
-		var processId int
-		var getWindowThreadProcessIDProcess = userModule.NewProc("GetWindowThreadProcessId")
-		getWindowThreadProcessIDProcess.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&processId)))
-		fmt.Printf("Lineage launcher PID: %d\n", processId)
+	if isFindWindow {
+		// get window Thread Process Id
+		windowProcessThreadID := getWindowThreadProcessID(windowHandle)
+		fmt.Printf("Lineage launcher PID: %d\n", windowProcessThreadID)
+		// Open Process
+		isOpenSuccess, handle := openProcess(windowProcessThreadID)
 
-		// OpenProcess
-		var openProcessProcess = kernelModule.NewProc("OpenProcess")
-		hwnd, _, _ := openProcessProcess.Call(uintptr(PROCESS_ALL_ACCESS), uintptr(0), uintptr(processId))
-
-		if hwnd <= 0 {
-			fmt.Println("Could not get handle!")
-			fmt.Println("請使用系統管理員啟動!")
-		} else {
-			var data byte
+		if isOpenSuccess {
 			var address uint32 = 0x5574a6 // data address
 			var offset uint32 = 0x02      // offset
 			fmt.Println()
@@ -45,22 +27,63 @@ func main() {
 			// Read data
 			for {
 				// Read Process Memory
-				var readProcessMemoryProcess = kernelModule.NewProc("ReadProcessMemory")
-				readProcessMemoryProcess.Call(uintptr(hwnd), uintptr(address+offset), uintptr(unsafe.Pointer(&data)), unsafe.Sizeof(data), 0)
+				data := readProcessMemory(handle, address+offset)
 				fmt.Print(string(data))
-				// get another byte
-				offset += 0x01
 				// end of data
 				if data == 0 {
 					fmt.Println()
 					break
 				}
+				// get another byte
+				offset += 0x01
 			}
 			fmt.Println()
 			fmt.Println("Developed by L1J-TW")
+		} else {
+			fmt.Println("Could not get handle!")
+			fmt.Println("請使用系統管理員啟動!")
 		}
+	} else {
+		fmt.Println("Window not found!")
 	}
 	// Wait for Enter
 	fmt.Print("Press 'Enter' to continue...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func readProcessMemory(handle uintptr, address uint32) byte {
+	var data byte
+	var kernelModule = syscall.NewLazyDLL("kernel32.dll")
+	var readProcessMemoryProcess = kernelModule.NewProc("ReadProcessMemory")
+	readProcessMemoryProcess.Call(uintptr(handle), uintptr(address), uintptr(unsafe.Pointer(&data)), unsafe.Sizeof(data), 0)
+	return data
+}
+
+func openProcess(processID int) (bool, uintptr) {
+	const processAllAccess = 0x1f0fff
+	var kernelModule = syscall.NewLazyDLL("kernel32.dll")
+	var openProcessProcess = kernelModule.NewProc("OpenProcess")
+	hwnd, _, _ := openProcessProcess.Call(uintptr(processAllAccess), uintptr(0), uintptr(processID))
+	return hwnd != 0, hwnd
+}
+
+func getWindowThreadProcessID(windowHandle uintptr) int {
+	// get window Thread Process id
+	var windowProcessID int
+	// Load dll
+	var userModule = syscall.NewLazyDLL("user32.dll")
+	// Create Process
+	var getWindowThreadProcessIDProcess = userModule.NewProc("GetWindowThreadProcessId")
+	getWindowThreadProcessIDProcess.Call(uintptr(windowHandle), uintptr(unsafe.Pointer(&windowProcessID)))
+	return windowProcessID
+}
+
+func findWindow(windowName string) (bool, uintptr) {
+	// Load dll
+	var userModule = syscall.NewLazyDLL("user32.dll")
+	// Create Process
+	var findWindowProcess = userModule.NewProc("FindWindowW")
+	// Call FindWindowW windows api
+	result, _, _ := findWindowProcess.Call(0, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(windowName))))
+	return result != 0, result
 }
